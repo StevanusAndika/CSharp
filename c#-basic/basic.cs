3ad617e0b726de39abe109dbbeb39e47a3b9de70
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-09-11 13:23:44 +0700

    Create basic.cs

---
+++ c#-basic/basic.cs
@@ -0,0 +1,13 @@
+// 1. Deklarasi pustaka
+
+using System;
+
+// 2.bagian class/blueprint 
+class FiratProgram{
+
+    // // 3. Bagian Fungsi yang akan dieksekusi
+    static void Main(String[] args)
+    {
+        // console.writeline untuk menampilkan output
+        Console.WriteLine("Belajar c#!");
+    }
